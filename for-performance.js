1d2589e234ec0be08d5d2b65cf117c31594b28e2
Mark Bailey <markbailey0356@gmail.com>
2018-09-03 23:35:54 +0930

    Add for loop and console output for var loop

--- for-performance.js
+++ for-performance.js
@@ -1,3 +1,8 @@
+"use strict";
 var n = 10000;  // number of for loop iterations
 // execution time of for loop with var counter
 var startTime = Date.now();
+for (var i = 0; i < n; i++) {
+}
+var endTime = Date.now();
+console.log("Execution time for " + n + " iterations using var counter: " + (endTime - startTime));
