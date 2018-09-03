7fa8976c2f01f5e033715ec8e5fc193a5cd82b8f
Mark Bailey <markbailey0356@gmail.com>
2018-09-04 00:10:34 +0930

    Use IIFEs to remove slow global object lookup in var cases

--- for-performance.js
+++ for-performance.js
@@ -1,36 +1,43 @@
 "use strict";
 var n = 1e9;  // number of for loop iterations
 var startTime, endTime;
-var a; // global variable to assign to
 
 // execution time of for loop with var counter with var assignment
-startTime = Date.now();
-for (var i = 0; i < n; i++) {
-  a = i;
-}
-endTime = Date.now();
-console.log("Execution time for " + n + " iterations using var counter and var assignment: " + (endTime - startTime));
+(function() {
+  startTime = Date.now();
+  for (var i = 0; i < n; i++) {
+    var a = i;
+  }
+  endTime = Date.now();
+  console.log("Execution time for " + n + " iterations using var counter and var assignment: " + (endTime - startTime));
+})();
 
 // execution time of for loop with let counter with var assignment
+(function() {
 startTime = Date.now();
 for (let i = 0; i < n; i++) {
-  a = i;
+  var a = i;
 }
 endTime = Date.now();
 console.log("Execution time for " + n + " iterations using let counter and var assignment: " + (endTime - startTime));
+})();
 
 // execution time of for loop with var counter with let assignment
+(function() {
 startTime = Date.now();
 for (var i = 0; i < n; i++) {
   let a = i;
 }
 endTime = Date.now();
 console.log("Execution time for " + n + " iterations using var counter and let assignment: " + (endTime - startTime));
+})();
 
 // execution time of for loop with let counter with let assignment
+(function() {
 startTime = Date.now();
 for (let i = 0; i < n; i++) {
   let a = i;
 }
 endTime = Date.now();
 console.log("Execution time for " + n + " iterations using let counter and let assignment: " + (endTime - startTime));
+})();
