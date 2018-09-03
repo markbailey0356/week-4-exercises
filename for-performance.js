2008215dd0c77fd912182cbb6754a99e3208094b
Mark Bailey <markbailey0356@gmail.com>
2018-09-04 00:04:43 +0930

    Add timers using let assignment at each operation

--- for-performance.js
+++ for-performance.js
@@ -3,18 +3,34 @@ var n = 1e9;  // number of for loop iterations
 var startTime, endTime;
 var a; // global variable to assign to
 
-// execution time of for loop with var counter
+// execution time of for loop with var counter with var assignment
 startTime = Date.now();
 for (var i = 0; i < n; i++) {
   a = i;
 }
 endTime = Date.now();
-console.log("Execution time for " + n + " iterations using var counter: " + (endTime - startTime));
+console.log("Execution time for " + n + " iterations using var counter and var assignment: " + (endTime - startTime));
 
-// execution time of for loop with let counter
+// execution time of for loop with let counter with var assignment
 startTime = Date.now();
 for (let i = 0; i < n; i++) {
   a = i;
 }
 endTime = Date.now();
-console.log("Execution time for " + n + " iterations using let counter: " + (endTime - startTime));
+console.log("Execution time for " + n + " iterations using let counter and var assignment: " + (endTime - startTime));
+
+// execution time of for loop with var counter with let assignment
+startTime = Date.now();
+for (var i = 0; i < n; i++) {
+  let a = i;
+}
+endTime = Date.now();
+console.log("Execution time for " + n + " iterations using var counter and let assignment: " + (endTime - startTime));
+
+// execution time of for loop with let counter with let assignment
+startTime = Date.now();
+for (let i = 0; i < n; i++) {
+  let a = i;
+}
+endTime = Date.now();
+console.log("Execution time for " + n + " iterations using let counter and let assignment: " + (endTime - startTime));
