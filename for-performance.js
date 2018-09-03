ae2cfe5ed86bfc93d7f93073d1d3ddbc5fdcbb85
Mark Bailey <markbailey0356@gmail.com>
2018-09-03 23:37:41 +0930

    Implement execution time counter for let loop

--- for-performance.js
+++ for-performance.js
@@ -1,8 +1,16 @@
 "use strict";
 var n = 10000;  // number of for loop iterations
+
 // execution time of for loop with var counter
 var startTime = Date.now();
 for (var i = 0; i < n; i++) {
 }
 var endTime = Date.now();
 console.log("Execution time for " + n + " iterations using var counter: " + (endTime - startTime));
+
+// execution time of for loop with let counter
+startTime = Date.now();
+for (let i = 0; i < n; i++) {
+}
+endTime = Date.now();
+console.log("Execution time for " + n + " iterations using let counter: " + (endTime - startTime));
