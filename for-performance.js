fc768a2e86f7527c9a266b12ba1498dcc007d47f
Mark Bailey <markbailey0356@gmail.com>
2018-09-03 23:56:15 +0930

    Add non-trivial assignment expression to each loop

--- for-performance.js
+++ for-performance.js
@@ -1,10 +1,12 @@
 "use strict";
 var n = 1e9;  // number of for loop iterations
 var startTime, endTime;
+var a; // global variable to assign to
 
 // execution time of for loop with var counter
 startTime = Date.now();
 for (var i = 0; i < n; i++) {
+  a = i;
 }
 endTime = Date.now();
 console.log("Execution time for " + n + " iterations using var counter: " + (endTime - startTime));
@@ -12,6 +14,7 @@ console.log("Execution time for " + n + " iterations using var counter: " + (end
 // execution time of for loop with let counter
 startTime = Date.now();
 for (let i = 0; i < n; i++) {
+  a = i;
 }
 endTime = Date.now();
 console.log("Execution time for " + n + " iterations using let counter: " + (endTime - startTime));
